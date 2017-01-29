69e297a28ef7509eeaa387a045f3946464286295
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-01-29 16:13:48 +1100

    quote handler

---
+++ src/main/kotlin/me/serce/solidity/ide/quotes.kt
@@ -0,0 +1,11 @@
+package me.serce.solidity.ide
+
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.highlighter.HighlighterIterator
+import me.serce.solidity.lang.core.SolidityTokenTypes
+
+
+class SolidityQuoteTokenHandler : SimpleTokenSetQuoteHandler(SolidityTokenTypes.STRINGLITERAL) {
+  override fun hasNonClosedLiteral(editor: Editor?, iterator: HighlighterIterator?, offset: Int) = true
+}
