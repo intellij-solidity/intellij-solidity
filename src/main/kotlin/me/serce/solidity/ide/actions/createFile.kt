c46937f0067c7b0c471dec349bf105bef9224454
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-21 14:50:57 +0100

    Add ile templates for Smart contract and Library
    Register file templates
    Extend CreateFileAction

--- src/main/kotlin/me/serce/solidity/ide/actions/createFile.kt
+++ src/main/kotlin/me/serce/solidity/ide/actions/createFile.kt
@@ -4,10 +4,15 @@ import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.InputValidatorEx
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiDirectory
 import me.serce.solidity.ide.SolidityIcons
 
 private val CAPTION = "New Solidity File"
+private val CONTRACT_TEMPLATE = "Solidity Contract"
+private val LIBRARY_TEMPLATE = "Solidity Library"
 
 class SolidityCreateFileAction : CreateFileFromTemplateAction(CAPTION, "", SolidityIcons.FILE_ICON), DumbAware {
 
@@ -15,6 +20,24 @@ class SolidityCreateFileAction : CreateFileFromTemplateAction(CAPTION, "", Solid
 
   override fun buildDialog(project: Project?, directory: PsiDirectory?,
                            builder: CreateFileFromTemplateDialog.Builder) {
-    builder.setTitle(CAPTION).addKind("Empty File", SolidityIcons.FILE_ICON, "Solidity File")
+    builder.setTitle(CAPTION)
+      .addKind("Smart contract", SolidityIcons.FILE_ICON, CONTRACT_TEMPLATE)
+      .addKind("Solidity library", SolidityIcons.FILE_ICON, LIBRARY_TEMPLATE)
+      .setValidator(object : InputValidatorEx {
+        override fun checkInput(inputString: String): Boolean {
+          return getErrorText(inputString) == null
+        }
+
+        override fun canClose(inputString: String): Boolean {
+          return getErrorText(inputString) == null
+        }
+
+        override fun getErrorText(inputString: String): String? {
+          return if (!StringUtil.isEmpty(inputString) && FileUtil.sanitizeFileName(inputString, false) == inputString)
+            null
+          else
+            "'$inputString' is not a valid Solidity module name"
+        }
+      })
   }
 }
