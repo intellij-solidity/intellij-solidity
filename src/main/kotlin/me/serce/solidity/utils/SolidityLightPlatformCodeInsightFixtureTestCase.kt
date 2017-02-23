c61730bed2deb4a9cfc2c15903f95c50523fa3b4
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-23 22:05:22 +0100

    Add tests for formatter

---
+++ src/main/kotlin/me/serce/solidity/utils/SolidityLightPlatformCodeInsightFixtureTestCase.kt
@@ -0,0 +1,68 @@
+package me.serce.solidity.utils
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+import com.intellij.util.ObjectUtils
+import com.intellij.util.PlatformUtils
+import com.intellij.util.containers.ContainerUtil
+
+abstract class SolidityLightPlatformCodeInsightFixtureTestCase : LightPlatformCodeInsightFixtureTestCase {
+  private var myBackedUpPlatformPrefix: String? = null
+  private val myIsSmallIde: Boolean
+
+  protected constructor(isSmallIde: Boolean) {
+    myIsSmallIde = isSmallIde
+  }
+
+  protected constructor() {
+    myIsSmallIde = false
+  }
+
+  @Throws(Exception::class)
+  override fun setUp() {
+    if (myIsSmallIde) {
+      myBackedUpPlatformPrefix = PlatformUtils.getPlatformPrefix()
+      System.setProperty(PlatformUtils.PLATFORM_PREFIX_KEY, PlatformUtils.PYCHARM_PREFIX)
+    }
+    super.setUp()
+  }
+
+  @Throws(Exception::class)
+  override fun tearDown() {
+    if (myIsSmallIde) {
+      System.setProperty(PlatformUtils.PLATFORM_PREFIX_KEY, myBackedUpPlatformPrefix!!)
+    }
+    super.tearDown()
+  }
+
+  protected fun setUpProjectSdk() {
+    ApplicationManager.getApplication().runWriteAction {
+      val sdk = projectDescriptor.sdk
+      ProjectJdkTable.getInstance().addJdk(sdk)
+      ProjectRootManager.getInstance(myFixture.project).projectSdk = sdk
+    }
+  }
+
+  protected fun launchIntention(name: String) {
+    val availableIntentions = myFixture.filterAvailableIntentions(name)
+    val action = ContainerUtil.getFirstItem(availableIntentions)
+    assertNotNull(action)
+    myFixture.launchAction(action!!)
+  }
+
+  @JvmOverloads protected fun assertNoIntentionsAvailable(name: String, message: String? = null) {
+    val availableIntentions = myFixture.filterAvailableIntentions(name)
+    val action = ContainerUtil.getFirstItem(availableIntentions)
+    assertNull(message, action)
+  }
+
+  protected fun <T : PsiElement> getElementAtCaret(clazz: Class<T>): T {
+    val offset = myFixture.editor.caretModel.offset
+    val focused = myFixture.file.findElementAt(offset)
+    return ObjectUtils.assertNotNull(PsiTreeUtil.getParentOfType(focused, clazz))
+  }
+}
---
+++ src/test/kotlin/me/serce/solidity/ide/format.kt
@@ -0,0 +1,44 @@
+package me.serce.solidity.ide
+
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.psi.PsiElement
+import com.intellij.psi.codeStyle.CodeStyleManager
+
+import java.io.File
+
+import me.serce.solidity.utils.SolidityLightPlatformCodeInsightFixtureTestCase
+
+class SolidityFormattingTest : SolidityLightPlatformCodeInsightFixtureTestCase() {
+  @Throws(Exception::class)
+  private fun doTest() {
+    val inputFile = this.inputFileName
+    val inputText = FileUtil.loadFile(File(this.testDataPath + inputFile))
+    this.myFixture.configureByText(inputFile, inputText)
+    WriteCommandAction.runWriteCommandAction(this.myFixture.project, Runnable { CodeStyleManager.getInstance(this@SolidityFormattingTest.project).reformat(this@SolidityFormattingTest.myFixture.file as PsiElement) })
+    val outputFile = File(this.myFixture.testDataPath + "/" + this.expectedOutputFileName)
+    val expectedResultText = FileUtil.loadFile(outputFile, true)
+    this.myFixture.checkResult(expectedResultText)
+  }
+
+  private val inputFileName: String
+    get() = this.getTestName(true) + ".sol"
+
+  private val expectedOutputFileName: String
+    get() = this.getTestName(true) + "-after.sol"
+
+  @Throws(Exception::class)
+  fun testContract() {
+    this.doTest()
+  }
+
+  @Throws(Exception::class)
+  fun testLibrary() {
+    this.doTest()
+  }
+
+  override fun getTestDataPath(): String {
+    return "src/test/resources/fixtures/formatter/"
+  }
+
+}
