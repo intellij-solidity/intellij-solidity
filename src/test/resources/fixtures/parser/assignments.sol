3e630bbd79ca9438773d4aba35580760b53760e6
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-02-25 20:38:08 +1100

    grammar workaround for tuples

--- src/test/resources/fixtures/parser/assignments.sol
+++ src/test/resources/fixtures/parser/assignments.sol
@@ -1,11 +1,10 @@
 contract C {
     function g() {
         var (x, b, y) = f();
-        // TODO fix this
-        //(x, y) = (2, 7);
-        //(x, y) = (y, x);
-        //(data.length,) = f();
-        //(,data[3]) = f();
-        //(x,) = (1,);
+        (x, y) = (2, 7);
+        (x, y) = (y, x);
+        (,data[3]) = f();
+        (data.length,) = f();
+        (x,) = (1,);
     }
 }
