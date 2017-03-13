e2e3daf33059aaf33882d6508e63afb0a0358603
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-03-14 00:35:37 +1100

    fix import resolve

--- src/main/kotlin/me/serce/solidity/lang/core/psi.kt
+++ src/main/kotlin/me/serce/solidity/lang/core/psi.kt
@@ -1,13 +1,14 @@
 package me.serce.solidity.lang.core
 
-import com.intellij.psi.tree.IElementType
 import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.FileViewProvider
+import com.intellij.psi.tree.IElementType
 import me.serce.solidity.lang.SolidityFileType
 import me.serce.solidity.lang.SolidityLanguage
+import me.serce.solidity.lang.psi.SolElement
 
-class SolidityFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, SolidityLanguage) {
+class SolidityFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, SolidityLanguage), SolElement {
     override fun getFileType(): FileType = SolidityFileType
     override fun toString(): String = "Solidity File"
 }
