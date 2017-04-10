d1294bbbdeffb740b9a8830b632ddb256744543b
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-04-11 01:31:56 +1000

    release notes and bump stubs

--- src/main/kotlin/me/serce/solidity/lang/stubs/impl.kt
+++ src/main/kotlin/me/serce/solidity/lang/stubs/impl.kt
@@ -14,7 +14,7 @@ class SolidityFileStub(file: SolidityFile?) : PsiFileStubImpl<SolidityFile>(file
 
   object Type : IStubFileElementType<SolidityFileStub>(SolidityLanguage) {
     // bump version every time stub tree changes
-    override fun getStubVersion() = 7
+    override fun getStubVersion() = 8
 
     override fun getBuilder(): StubBuilder = object : DefaultStubBuilder() {
       override fun createStubForFile(file: PsiFile) = SolidityFileStub(file as SolidityFile)
