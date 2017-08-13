75bae0135e3ff6856a7864faba7a804215438a5e
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-08-13 20:47:05 +1000

    parameter info hint

---
+++ src/test/kotlin/me/serce/solidity/ide/hints/SolParameterInfoHandlerTest.kt
@@ -0,0 +1,75 @@
+package me.serce.solidity.ide.hints
+
+import com.intellij.testFramework.utils.parameterInfo.MockCreateParameterInfoContext
+import com.intellij.testFramework.utils.parameterInfo.MockParameterInfoUIContext
+import com.intellij.testFramework.utils.parameterInfo.MockUpdateParameterInfoContext
+import junit.framework.AssertionFailedError
+import junit.framework.TestCase
+import me.serce.solidity.utils.SolTestBase
+import org.intellij.lang.annotations.Language
+
+class SolParameterInfoHandlerTest : SolTestBase() {
+  fun testEmptyParameters() = checkByText("""
+        contract A {
+            function foo() {}
+
+            function main() {
+                foo(/*caret*/);
+            }
+        }
+    """, "<no arguments>", -1)
+
+  fun testInt() = checkByText("""
+        contract A {
+            function foo(uint256 a) {}
+
+            function main() {
+                foo(1/*caret*/);
+            }
+        }
+    """, "uint256 a", 0)
+
+  fun testMultipleParameter2() = checkByText("""
+        contract A {
+            function foo(uint256 a, int b) {}
+
+            function main() {
+                foo(1, 2/*caret*/);
+            }
+        }
+    """, "uint256 a, int b", 1)
+
+  fun testMultipleParameter3() = checkByText("""
+        contract A {
+            function foo(string a, int b, address c) {}
+
+            function main() {
+                foo("1"/*caret*/, 2, 0x000d);
+            }
+        }
+    """, "string a, int b, address c", 0)
+
+  private fun checkByText(@Language("Solidity") code: String, hint: String, index: Int) {
+    myFixture.configureByText("main.sol", code.replace("/*caret*/", "<caret>"))
+    val handler = SolParameterInfoHandler()
+    val createContext = MockCreateParameterInfoContext(myFixture.editor, myFixture.file)
+
+    val el = handler.findElementForParameterInfo(createContext)
+    if (hint.isNotEmpty()) {
+      el ?: throw AssertionFailedError("Hint not found")
+      handler.showParameterInfo(el, createContext)
+      val items = createContext.itemsToShow ?: throw AssertionFailedError("Parameters are not shown")
+      if (items.isEmpty()) throw AssertionFailedError("Parameters are empty")
+      val context = MockParameterInfoUIContext(el)
+      handler.updateUI(items[0] as SolArgumentsDescription, context)
+      TestCase.assertEquals(hint, handler.hintText)
+
+      val updateContext = MockUpdateParameterInfoContext(myFixture.editor, myFixture.file)
+      val element = handler.findElementForUpdatingParameterInfo(updateContext) ?: throw AssertionFailedError("Parameter not found")
+      handler.updateParameterInfo(element, updateContext)
+      TestCase.assertEquals(index, updateContext.currentParameter)
+    } else if (el != null) {
+      throw AssertionFailedError("Unexpected hint found")
+    }
+  }
+}
