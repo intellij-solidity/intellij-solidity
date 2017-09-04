ee892cbe9a666f0079b08a1777eaa1225efee93e
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-09-04 22:47:08 +1000

    Add support for ambiguous assembly instructions

--- src/test/resources/fixtures/parser/assembly.sol
+++ src/test/resources/fixtures/parser/assembly.sol
@@ -6,4 +6,20 @@ contract Foo {
         assembly { a := b() }
         assembly { =: b }
     }
+
+    function get() constant returns(uint) {
+        assembly { 2 3 add "abc" and }
+        assembly {
+            mstore(0x40, sload(0))
+            byte(0)
+            add(x, y)
+            selfdestruct(a)
+            keccak256(p, n)
+            jump(label)
+            sha3(p, n)
+            number(label)
+            address(0)
+            return(0x40,32)
+        }
+    }
 }
