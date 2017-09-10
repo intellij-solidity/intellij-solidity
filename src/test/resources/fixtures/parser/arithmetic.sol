b8eba46f9db3864f78249b9da88323dbe4c22e96
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-09-10 17:02:33 +1000

    fix a lexer conflict betwen XOR and PRAGMA

--- src/test/resources/fixtures/parser/arithmetic.sol
+++ src/test/resources/fixtures/parser/arithmetic.sol
@@ -1,8 +1,9 @@
 contract test {
   function f(uint x, uint y) returns (uint z) {
-    var c = (x) + 3;
-    var c = x + 3;
+    var a = (x) + 3;
     var b = 7 + (c * (8 - 7)) - x;
+    var c = x + 3;
+    var d = A^d;
     return -(-b | 0);
   }
 }
