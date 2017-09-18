67132035d33abd02850aceb0743aa329e94322a4
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-09-18 21:40:49 +1000

    Fix grammar for `new string(len)` expression

--- src/test/resources/fixtures/parser/newArray.sol
+++ src/test/resources/fixtures/parser/newArray.sol
@@ -1,5 +1,6 @@
 contract Foo {
     function Foo() {
         var a = new bytes32[](a);
+        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
     }
 }
