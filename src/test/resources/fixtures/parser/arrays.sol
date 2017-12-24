e2fb95b93f72259979e4fc27e7f19bbbedf10487
ZitRos <zitros.lab@gmail.com>
2017-12-24 20:03:51 +0200

    Allow boolean constructor

--- src/test/resources/fixtures/parser/arrays.sol
+++ src/test/resources/fixtures/parser/arrays.sol
@@ -3,6 +3,7 @@ contract A {
         uint lenght = 1;
         uint256[] memory result = new uint[](lenght);
         address[] memory ads = new address[](5);
+        bool[] memory boolean = new bool[](2);
         bytes memory fetchedTokenByteCode = du();
         string memory abcde = "hello";
     }
