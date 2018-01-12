0b8c2e0d778cb692e196fa4434d383ab62bb7471
0v1se <dev@0v1se.com>
2018-01-12 15:09:25 +0300

    fix for constructor rename (#40)

---
+++ src/main/kotlin/me/serce/solidity/ide/refactoring/RenameConstructorProcessor.kt
@@ -0,0 +1,16 @@
+package me.serce.solidity.ide.refactoring
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.SearchScope
+import com.intellij.refactoring.rename.RenamePsiElementProcessor
+import me.serce.solidity.lang.psi.SolFunctionDefinition
+
+class RenameConstructorProcessor : RenamePsiElementProcessor() {
+  override fun canProcessElement(element: PsiElement): Boolean {
+    return element is SolFunctionDefinition && element.isConstructor
+  }
+
+  override fun prepareRenaming(element: PsiElement?, newName: String?, allRenames: MutableMap<PsiElement, String>?, scope: SearchScope?) {
+     allRenames?.put((element as SolFunctionDefinition).parent, newName!!)
+  }
+}
