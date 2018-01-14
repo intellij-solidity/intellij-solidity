16a08e736d4ea9d231239d2afc889b7b8575704f
Sergey Tselovalnikov <sergeicelov@gmail.com>
2018-01-14 13:33:30 +1100

    auto-ident inside assembly blocks

--- src/main/kotlin/me/serce/solidity/ide/formatting/SolFormattingBlock.kt
+++ src/main/kotlin/me/serce/solidity/ide/formatting/SolFormattingBlock.kt
@@ -79,7 +79,7 @@ class SolFormattingBlock(private val astNode: ASTNode,
   }
 
   private fun newChildIndent(childIndex: Int): Indent? = when {
-    node.elementType in listOf(BLOCK, CONTRACT_DEFINITION, STRUCT_DEFINITION, ENUM_DEFINITION) -> {
+    node.elementType in listOf(BLOCK, ASSEMBLY_BLOCK, CONTRACT_DEFINITION, STRUCT_DEFINITION, ENUM_DEFINITION) -> {
       val lbraceIndex = subBlocks.indexOfFirst { it is ASTBlock && it.node.elementType == LBRACE }
       if (lbraceIndex != -1 && lbraceIndex < childIndex) {
         Indent.getNormalIndent()
