e31106101ad1cf1db6dd94d1cbebd3b78cc70c62
0v1se <dev@0v1se.com>
2018-01-17 17:06:35 +0300

    truffle/npm import support (#42)

--- src/main/kotlin/me/serce/solidity/lang/resolve/ref/SolImportPathReference.kt
+++ src/main/kotlin/me/serce/solidity/lang/resolve/ref/SolImportPathReference.kt
@@ -1,5 +1,6 @@
 package me.serce.solidity.lang.resolve.ref
 
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.psi.impl.source.tree.LeafElement
@@ -13,11 +14,30 @@ class SolImportPathReference(element: SolImportPathElement) : SolReferenceBase<S
       return null
     }
     val path = importText.substring(1, importText.length - 1)
-    val file = element.containingFile.virtualFile.findFileByRelativePath("../$path")
-    if (file == null) {
+    val file = findImportFile(element.containingFile.virtualFile, path)
+    if (file != null) {
+      return PsiManager.getInstance(element.project).findFile(file)
+    } else {
       return null
     }
-    return PsiManager.getInstance(element.project).findFile(file)
+  }
+
+  private fun findImportFile(file: VirtualFile, path: String): VirtualFile? {
+    val directFile = file.findFileByRelativePath("../$path")
+    if (directFile != null) {
+      return directFile
+    } else {
+      return findNpmImportFile(file, path)
+    }
+  }
+
+  private fun findNpmImportFile(file: VirtualFile, path: String): VirtualFile? {
+    val test = file.findFileByRelativePath("node_modules/$path")
+    return when {
+        test != null -> test
+        file.parent != null -> findNpmImportFile(file.parent, path)
+        else -> null
+    }
   }
 
   override fun doRename(identifier: PsiElement, newName: String) {
--- src/test/kotlin/me/serce/solidity/lang/core/resolve/SolImportResolveTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/core/resolve/SolImportResolveTest.kt
@@ -25,6 +25,19 @@ class SolImportResolveTest : SolResolveTestBase() {
     assertEquals(file1.name, resolved.containingFile.name)
   }
 
+  fun testImportPathResolveNpm() {
+    val file1 = myFixture.configureByFile("node_modules/util/contracts/TestImport.sol");
+    myFixture.configureByFile("contracts/ImportUsage.sol");
+
+    val (refElement) = findElementAndDataInEditor<SolNamedElement>("^")
+
+    val resolved = checkNotNull(refElement.reference?.resolve()) {
+      "Failed to resolve ${refElement.text}"
+    }
+
+    assertEquals(file1.name, resolved.containingFile.name)
+  }
+
   fun testResolveFrom() {
     val file1 = InlineFile(
       name = "Ownable.sol",
@@ -46,4 +59,6 @@ class SolImportResolveTest : SolResolveTestBase() {
 
     assertEquals(file1.name, resolved.containingFile.name)
   }
+
+  override fun getTestDataPath() = "src/test/resources/fixtures/import/"
 }
