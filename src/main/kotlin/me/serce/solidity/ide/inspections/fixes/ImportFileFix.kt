34a2f65db6d33c3245534fd0224cc638181e5fea
0v1se <dev@0v1se.com>
2018-02-21 13:18:07 +0100

    NPE fix

--- src/main/kotlin/me/serce/solidity/ide/inspections/fixes/ImportFileFix.kt
+++ src/main/kotlin/me/serce/solidity/ide/inspections/fixes/ImportFileFix.kt
@@ -15,40 +15,47 @@ import me.serce.solidity.lang.psi.SolUserDefinedTypeName
 import me.serce.solidity.lang.resolve.SolResolver
 
 class ImportFileFix(element: SolUserDefinedTypeName): LocalQuickFixOnPsiElement(element), HintAction, LocalQuickFix {
-  val element:SolUserDefinedTypeName
-    get() = startElement as SolUserDefinedTypeName
-
   override fun startInWriteAction(): Boolean = false
 
   override fun getFamilyName(): String =
     QuickFixBundle.message("import.class.fix")
 
   override fun showHint(editor: Editor): Boolean {
-    val suggestions = SolResolver.resolveTypeName(element).map { it.containingFile }.toSet()
-    val fixText: String? = if (suggestions.size == 1) {
-      val importPath = buildImportPath(element.containingFile.virtualFile, suggestions.first().virtualFile)
-      QuickFixBundle.message("import.class.fix") + " $importPath"
-    } else if (suggestions.isNotEmpty()) {
-      QuickFixBundle.message("import.class.fix")
-    } else {
-      null
-    }
-    if (fixText != null) {
-      HintManager.getInstance().showQuestionHint(editor, fixText, element.textOffset, element.getTextRange().getEndOffset(), ImportFileAction(editor, element.containingFile, suggestions))
-      return true
+    val element = startElement as SolUserDefinedTypeName?
+    if (element != null) {
+      val suggestions = SolResolver.resolveTypeName(element).map { it.containingFile }.toSet()
+      val fixText: String? = if (suggestions.size == 1) {
+        val importPath = buildImportPath(element.containingFile.virtualFile, suggestions.first().virtualFile)
+        QuickFixBundle.message("import.class.fix") + " $importPath"
+      } else if (suggestions.isNotEmpty()) {
+        QuickFixBundle.message("import.class.fix")
+      } else {
+        null
+      }
+      if (fixText != null) {
+        HintManager.getInstance().showQuestionHint(editor, fixText, element.textOffset, element.getTextRange().getEndOffset(), ImportFileAction(editor, element.containingFile, suggestions))
+        return true
+      } else {
+        return false
+      }
     } else {
       return false
     }
   }
 
   override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
-    if (!element.isValid) {
+    val element = startElement as SolUserDefinedTypeName?
+    if (element != null) {
+      if (!element.isValid) {
+        return false
+      }
+      if (element.reference?.resolve() != null) {
+        return false
+      }
+      return SolResolver.resolveTypeName(element).isNotEmpty()
+    } else {
       return false
     }
-    if (element.reference?.resolve() != null) {
-      return false
-    }
-    return SolResolver.resolveTypeName(element).isNotEmpty()
   }
 
   override fun getText(): String =
@@ -62,9 +69,12 @@ class ImportFileFix(element: SolUserDefinedTypeName): LocalQuickFixOnPsiElement(
   }
 
   override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-    val suggestions = SolResolver.resolveTypeName(element as SolReferenceElement).map { it.containingFile }.toSet()
-    if (suggestions.size == 1) {
-      ImportFileAction.addImport(project, element.containingFile, suggestions.first())
+    val element = startElement as SolUserDefinedTypeName?
+    if (element != null) {
+      val suggestions = SolResolver.resolveTypeName(element as SolReferenceElement).map { it.containingFile }.toSet()
+      if (suggestions.size == 1) {
+        ImportFileAction.addImport(project, element.containingFile, suggestions.first())
+      }
     }
   }
 }
