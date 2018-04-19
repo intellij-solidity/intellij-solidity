2289ecd89114e28906994dc1f35ac14537997b1b
mbolotov <mbolotov@devexperts.com>
2018-04-19 14:42:31 +0300

    java-stubs: resolve git conflicts, handle constructor arguments

--- src/main/kotlin/me/serce/solidity/ide/interop/JavaStubGenerator.kt
+++ src/main/kotlin/me/serce/solidity/ide/interop/JavaStubGenerator.kt
@@ -11,8 +11,8 @@ object JavaStubGenerator {
   const val autoGenComment = "// Auto-generated code"
 
 
-  private fun contractStubTemplate(className: String, functions: List<SolFunctionDefinition>): String {
-    return """$autoGenComment
+  private fun contractStubTemplate(className: String, functions: List<SolFunctionDefinition>): String =
+    """$autoGenComment
 package $packageName;
 
 import org.ethereum.util.blockchain.SolidityContract;
@@ -24,24 +24,27 @@ public class $className {
 	}
 ${functions.joinToString("\n") { funcStubTemplate(it) }}
 }"""
-  }
 
   private fun funcStubTemplate(function: SolFunctionDefinition): String {
     val params = stringifyParams(function)
-    val paramNames = if (function.parameters.isNotEmpty()) {
-      ", " + function.parameters.map { it.name }.joinToString(", ")
-    } else ""
+    val paramRefs = paramRefs(function)
     val methodName = function.name
     return """
   public Object $methodName($params) {
-		return contract.callFunction("$methodName"$paramNames).getReturnValue();
+		return contract.callFunction("$methodName"$paramRefs).getReturnValue();
 	}
 """
   }
 
-  private fun contractRepoTemplate(contracts: List<CompiledContractDefinition>): String {
+  private fun paramRefs(function: SolFunctionDefinition?): String {
+    val parameters = function?.parameters ?: return ""
+    return if (parameters.isNotEmpty()) {
+      ", " + parameters.map { it.name }.joinToString(", ")
+    } else ""
+  }
 
-    return """$autoGenComment
+  private fun contractRepoTemplate(contracts: List<CompiledContractDefinition>): String =
+    """$autoGenComment
 package $packageName;
 
 import org.ethereum.config.SystemProperties;
@@ -53,50 +56,51 @@ import org.ethereum.util.blockchain.StandaloneBlockchain;
 import java.math.BigInteger;
 
 public class $repoClassName {
-    private EasyBlockchain blockchain;
-    private $repoClassName(EasyBlockchain blockchain) {
-        this.blockchain = blockchain;
-    }
+  private EasyBlockchain blockchain;
+  private $repoClassName(EasyBlockchain blockchain) {
+      this.blockchain = blockchain;
+  }
 
-    private static StandaloneBlockchain defaultBlockchain;
+  private static StandaloneBlockchain defaultBlockchain;
 
-    public static $repoClassName getInstance(EasyBlockchain blockchain) {
-        return new $repoClassName(blockchain);
-    }
+  public static $repoClassName getInstance(EasyBlockchain blockchain) {
+      return new $repoClassName(blockchain);
+  }
 
-    public static synchronized $repoClassName getInstance() {
-        if (defaultBlockchain == null) {
-            defaultBlockchain = initDefaultBlockchain();
-        }
-        return new $repoClassName(defaultBlockchain);
-    }
+  public static synchronized $repoClassName getInstance() {
+      if (defaultBlockchain == null) {
+          defaultBlockchain = initDefaultBlockchain();
+      }
+      return new $repoClassName(defaultBlockchain);
+  }
 
-    private static StandaloneBlockchain initDefaultBlockchain() {
-        SystemProperties.getDefault().setBlockchainConfig(new FrontierConfig(new FrontierConfig.FrontierConstants() {
-            @Override
-            public BigInteger getMINIMUM_DIFFICULTY() {
-                return BigInteger.ONE;
-            }
-        }));
-        StandaloneBlockchain blockchain = new StandaloneBlockchain().withAutoblock(true);
-        System.out.println("Creating first empty block (need some time to generate DAG)...");
-        blockchain.createBlock();
-        System.out.println("Done.");
-        return blockchain;
-    }
+  private static StandaloneBlockchain initDefaultBlockchain() {
+      SystemProperties.getDefault().setBlockchainConfig(new FrontierConfig(new FrontierConfig.FrontierConstants() {
+          @Override
+          public BigInteger getMINIMUM_DIFFICULTY() {
+              return BigInteger.ONE;
+          }
+      }));
+      StandaloneBlockchain blockchain = new StandaloneBlockchain().withAutoblock(true);
+      System.out.println("Creating first empty block (need some time to generate DAG)...");
+      blockchain.createBlock();
+      System.out.println("Done.");
+      return blockchain;
+  }
 
-    public EasyBlockchain getBlockchain() {
-        return blockchain;
-    }
+  public EasyBlockchain getBlockchain() {
+      return blockchain;
+  }
 
 ${contracts.joinToString("\n") { submitContractTemplate(it) }}
 }"""
-  }
 
   private fun submitContractTemplate(contract: CompiledContractDefinition): String {
     val name = contract.contract.name
     val definition = contract.contract
-    val params = stringifyParams(definition.functionDefinitionList.find { it.isConstructor })
+    val constructor = definition.functionDefinitionList.find { it.isConstructor }
+    val params = stringifyParams(constructor)
+    val paramRefs = paramRefs(constructor)
     return """
     public $name submit$name($params) {
         CompilationResult.ContractMetadata metadata = new CompilationResult.ContractMetadata();
@@ -106,7 +110,7 @@ ${contracts.joinToString("\n") { submitContractTemplate(it) }}
         } catch (Exception e) {
            throw new RuntimeException(e);
         }
-        return new $name(blockchain.submitNewContract(metadata));
+        return new $name(blockchain.submitNewContract(metadata$paramRefs));
     }
 """
   }
