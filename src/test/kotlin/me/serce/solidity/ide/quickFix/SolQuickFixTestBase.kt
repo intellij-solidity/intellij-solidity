cc84ee002f3c3d3e67a0fae5cf9d6faa08031961
Sergey Tselovalnikov <sergeicelov@gmail.com>
2018-04-28 15:12:47 +1000

    test: add a test for file import hint (#64)

--- src/test/kotlin/me/serce/solidity/ide/quickFix/SolQuickFixTestBase.kt
+++ src/test/kotlin/me/serce/solidity/ide/quickFix/SolQuickFixTestBase.kt
@@ -2,15 +2,22 @@ package me.serce.solidity.ide.quickFix
 
 import com.intellij.lang.annotation.HighlightSeverity
 import me.serce.solidity.utils.SolTestBase
+import org.intellij.lang.annotations.Language
 
 abstract class SolQuickFixTestBase : SolTestBase() {
-  fun testQuickFix(text: String, expected: String) {
-    InlineFile(text)
+  fun testQuickFix(@Language("Solidity") code: String, expected: String) {
+    InlineFile(code)
     val errors = myFixture.doHighlighting(HighlightSeverity.WARNING)
     assertEquals(1, errors.size)
     for (quickFixActionRange in errors[0].quickFixActionRanges!!) {
       quickFixActionRange.first.action.invoke(myFixture.project, myFixture.editor, myFixture.file)
     }
     myFixture.checkResult(expected)
+  }
+
+  fun assertNoQuickFix(@Language("Solidity") code: String) {
+    InlineFile(code)
+    val errors = myFixture.doHighlighting(HighlightSeverity.WARNING)
+    assertEquals(0, errors.size)
   }
 }
