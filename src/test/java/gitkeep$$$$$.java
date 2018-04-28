ec8af6bcc49e540ee5642a3cdfa6aca9c7ecb8fb
Sergey Tselovalnikov <sergeicelov@gmail.com>
2018-04-28 16:44:35 +1000

    Upgrade gradle to 4.7

---
+++ src/test/java/gitkeep$$$$$.java
@@ -0,0 +1,10 @@
+public class gitkeep$$$$$ {
+    /*
+     * Gradle complains
+     * * What went wrong:
+     *   Execution failed for task ':testClasses'.
+     *   > destination directory "/Users/serce/git/intellij-solidity/build/classes/java/test" does not exist or is not a directory
+     *
+     * Ok, gralde, now you're getting this directory and this empty test file
+     */
+}
