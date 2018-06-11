c23d629b726bd7e9732f397ba5140499bafd00ca
Sergey Tselovalnikov <sergeicelov@gmail.com>
2018-06-11 13:33:36 +1000

    grammar: add support for multi-variable declaration
    fix #96

--- src/test/resources/fixtures/parser/destructuring.sol
+++ src/test/resources/fixtures/parser/destructuring.sol
@@ -1,5 +1,13 @@
 contract MyToken {
     function MyToken() {
         var (value, fake, _, secret) = (_values[i], _fake[i], _secret[i], 6);
+        var (a, b,,) = f();
+        (bytes memory a, string storage b) = g();
+        (uint x1, S storage y1, uint z1) = g();
+        (uint x1, uint y1, uint z1) = (1, 2, 3);
+        (, S storage y2,) = g();
+        (uint x2,,) = g();
+        (,,uint z2) = g();
+        (,,,D.S[10*2] storage x,) = f();
     }
 }
