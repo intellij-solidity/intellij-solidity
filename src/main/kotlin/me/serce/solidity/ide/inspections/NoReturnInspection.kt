6fad20cf63c53bf7de7232cb110adc7d1f8fb3cf
0v1se <dev@0v1se.com>
2018-09-21 16:41:23 +0300

    no return statement inspection

--- src/main/kotlin/me/serce/solidity/ide/inspections/NoReturnInspection.kt
+++ src/main/kotlin/me/serce/solidity/ide/inspections/NoReturnInspection.kt
@@ -9,44 +9,105 @@ class NoReturnInspection : LocalInspectionTool() {
   override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
     return object : SolVisitor() {
       override fun visitFunctionDefinition(o: SolFunctionDefinition) {
-        val block = o.block
-        if (block != null) {
-          val returns = o.returns
-          if (returns != null) {
-            val shouldReturn = returns.parameterDefList.any { it.name == null }
-            if (shouldReturn && !block.returns) {
-              holder.registerProblem(o, "no return statement")
+        o.inspectReturns(holder)
+      }
+    }
+  }
+}
+
+fun SolFunctionDefinition.inspectReturns(holder: ProblemsHolder) {
+  val block = this.block
+  if (block != null) {
+    val returns = this.returns
+    if (returns != null) {
+      if (!block.returns) {
+        val shouldReturn = returns.parameterDefList.any { it.name == null }
+        if (shouldReturn) {
+          holder.registerProblem(this, "no return statement")
+        } else {
+          for (returnParam in returns.parameterDefList) {
+            if (!block.hasAssignment(returnParam.name!!)) {
+              holder.registerProblem(returnParam, "return variable not assigned")
             }
           }
         }
       }
     }
   }
+}
 
-  val SolBlock.returns: Boolean
-    get() = this.statementList.any { it.returns }
+fun SolStatement.hasAssignment(name: String): Boolean {
+  this.variableDefinition.let {
+    if (it != null) return it.hasAssignment(name)
+  }
 
-  val SolIfStatement.returns: Boolean
-    get() = if (this.statementList.size == 1) {
-      false
-    } else {
-      statementList.all { it.returns }
-    }
-
-  val SolStatement.returns: Boolean
-    get() {
-      this.block.let {
-        if (it != null) return it.returns
-      }
-      this.ifStatement.let {
-        if (it != null) return it.returns
-      }
-      if (this.returnSt != null) {
-        return true
-      }
-      if (this.returnTupleStatement != null) {
-        return true
+  this.expression.let {
+    if (it != null && it is SolAssignmentExpression) {
+      val first = it.expressionList[0]
+      if (first is SolPrimaryExpression) {
+        first.varLiteral.let {lit ->
+          if (lit != null) {
+            if (lit.name == name) {
+              return true
+            }
+          }
+        }
       }
       return false
     }
+  }
+
+  this.block.let {
+    if (it != null) return it.hasAssignment(name)
+  }
+
+  this.ifStatement.let {
+    if (it != null) return it.hasAssignment(name)
+  }
+
+  return false
 }
+
+val SolStatement.returns: Boolean
+  get() {
+    this.block.let {
+      if (it != null) return it.returns
+    }
+
+    this.ifStatement.let {
+      if (it != null) return it.returns
+    }
+
+    if (this.returnSt != null) {
+      return true
+    }
+
+    if (this.returnTupleStatement != null) {
+      return true
+    }
+
+    return false
+  }
+
+val SolBlock.returns: Boolean
+  get() = this.statementList.any { it.returns }
+
+fun SolBlock.hasAssignment(name: String): Boolean =
+  this.statementList.any { it.hasAssignment(name) }
+
+val SolIfStatement.returns: Boolean
+  get() = if (this.statementList.size == 1) {
+    false
+  } else {
+    statementList.all { it.returns }
+  }
+
+fun SolIfStatement.hasAssignment(name: String): Boolean =
+  if (this.statementList.size == 1) {
+    false
+  } else {
+    statementList.all { it.hasAssignment(name) }
+  }
+
+fun SolVariableDefinition.hasAssignment(name: String): Boolean =
+  this.variableDeclaration.name == name
--- src/test/kotlin/me/serce/solidity/ide/inspections/NoReturnInspectionTest.kt
+++ src/test/kotlin/me/serce/solidity/ide/inspections/NoReturnInspectionTest.kt
@@ -1,11 +1,51 @@
 package me.serce.solidity.ide.inspections
 
 class NoReturnInspectionTest : SolInspectionsTestBase(NoReturnInspection()) {
-  fun test() = checkByText("""
+  fun testNoReturnVarName() = checkByText("""
         contract a {
-            /*@warning descr="no return statement"@*/function a() returns (bool) {
+            /*@warning descr="no return statement"@*/function a() returns (uint) {
                 var test = 5;
             }/*@/warning@*/
+        }
+    """)
+
+  fun testReturnWithIf() = checkByText("""
+        contract a {
+            function a() returns (uint) {
+                if (true) {
+                  return 1;
+                } else {
+                  return 2;
+                }
+            }
+        }
+    """)
+
+  fun testAssignmentWithIf() = checkByText("""
+        contract a {
+            function a() returns (uint result) {
+                if (true) {
+                  result = 1;
+                } else {
+                  result = 2;
+                }
+            }
+        }
+    """)
+
+  fun testHasReturnVarName() = checkByText("""
+        contract a {
+            function a() returns (/*@warning descr="return variable not assigned"@*/uint a/*@/warning@*/) {
+                var test = 5;
+            }
+        }
+    """)
+
+  fun testHasReturnVarNameWithVarDef() = checkByText("""
+        contract a {
+            function a() returns (uint a) {
+                var a = 5;
+            }
         }
     """)
 }
