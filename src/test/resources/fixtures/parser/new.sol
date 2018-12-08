ce785e7d07ca7cc641fe7812f7ef9175085ab1e4
Sergey Tselovalnikov <sergeicelov@gmail.com>
2018-12-08 19:32:38 +1100

    grammar: highlight add new expression parsing (fix #170)

--- src/test/resources/fixtures/parser/new.sol
+++ src/test/resources/fixtures/parser/new.sol
@@ -4,3 +4,14 @@ contract Foo {
         string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
     }
 }
+
+contract King {
+    function King() public payable {
+    }
+}
+
+contract a {
+    function b() {
+        var k = new King;
+    }
+}
