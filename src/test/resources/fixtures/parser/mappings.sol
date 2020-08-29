742e5505cb9bd5986bc7ed153a6f37a2aea8bcbb
Sergey Tselovalnikov <sergeicelov@gmail.com>
2020-08-29 13:46:14 +1000

    fix #206: allow for typenames in mappings

--- src/test/resources/fixtures/parser/mappings.sol
+++ src/test/resources/fixtures/parser/mappings.sol
@@ -1,4 +1,7 @@
+interface IElement {}
+
 contract MyToken {
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
+    mapping (IElement => address) public testVar;
 }
