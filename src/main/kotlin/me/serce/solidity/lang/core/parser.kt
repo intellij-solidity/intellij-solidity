eb337f0550329793f262869998c5a8ad3204472b
Sergey Tselovalnikov <sergeicelov@gmail.com>
2021-09-06 15:56:04 +1000

    parser: override recursion guard

--- src/main/kotlin/me/serce/solidity/lang/core/parser.kt
+++ src/main/kotlin/me/serce/solidity/lang/core/parser.kt
@@ -1,9 +1,6 @@
 package me.serce.solidity.lang.core
 
-import com.intellij.lang.ASTNode
-import com.intellij.lang.LanguageUtil
-import com.intellij.lang.ParserDefinition
-import com.intellij.lang.PsiParser
+import com.intellij.lang.*
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.project.Project
 import com.intellij.psi.FileViewProvider
@@ -15,6 +12,19 @@ import com.intellij.psi.tree.TokenSet
 import me.serce.solidity.SolidityParser
 import me.serce.solidity.lang.core.SolidityTokenTypes.*
 import me.serce.solidity.lang.stubs.SolidityFileStub
+
+object ParserUtil {
+  const val MAX_RECURSION_LEVEL = 512
+
+  @JvmStatic
+  fun recursion_guard_(builder: PsiBuilder, level: Int, funcName: String): Boolean {
+    if (level > MAX_RECURSION_LEVEL) {
+      builder.mark().error("Maximum recursion level ($MAX_RECURSION_LEVEL) reached in '$funcName'")
+      return false
+    }
+    return true
+  }
+}
 
 class SolidityParserDefinition : ParserDefinition {
   override fun createParser(project: Project?): PsiParser = SolidityParser()
@@ -40,11 +50,39 @@ class SolidityParserDefinition : ParserDefinition {
     val WHITE_SPACES: TokenSet = TokenSet.create(TokenType.WHITE_SPACE)
     val COMMENTS: TokenSet = TokenSet.create(SolidityTokenTypes.COMMENT)
     val BINARY_OPERATORS: TokenSet = TokenSet.create(
-      PLUS, MINUS, MULT, DIV, EXPONENT,
-      ASSIGN, TO, EQ, NEQ,
-      PLUS_ASSIGN, MINUS_ASSIGN, MULT_ASSIGN, DIV_ASSIGN, OR_ASSIGN, XOR_ASSIGN, AND_ASSIGN, LSHIFT_ASSIGN, RSHIFT_ASSIGN, PERCENT_ASSIGN,
-      LESS, LESSEQ, MORE, MOREEQ, CARET, AND, ANDAND, OR, OROR,
-      PERCENT, LSHIFT, RSHIFT, LEFT_ASSEMBLY, RIGHT_ASSEMBLY
+      PLUS,
+      MINUS,
+      MULT,
+      DIV,
+      EXPONENT,
+      ASSIGN,
+      TO,
+      EQ,
+      NEQ,
+      PLUS_ASSIGN,
+      MINUS_ASSIGN,
+      MULT_ASSIGN,
+      DIV_ASSIGN,
+      OR_ASSIGN,
+      XOR_ASSIGN,
+      AND_ASSIGN,
+      LSHIFT_ASSIGN,
+      RSHIFT_ASSIGN,
+      PERCENT_ASSIGN,
+      LESS,
+      LESSEQ,
+      MORE,
+      MOREEQ,
+      CARET,
+      AND,
+      ANDAND,
+      OR,
+      OROR,
+      PERCENT,
+      LSHIFT,
+      RSHIFT,
+      LEFT_ASSEMBLY,
+      RIGHT_ASSEMBLY
     )
     val CONTROL_STRUCTURES: TokenSet = TokenSet.create(
       IF, ELSE, WHILE, FOR, DO
