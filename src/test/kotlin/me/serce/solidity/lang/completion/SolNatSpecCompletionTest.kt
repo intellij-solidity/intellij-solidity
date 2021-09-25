1088790618a3b0c98ae588f098404ebba5a83a14
Sergey Tselovalnikov <sergeicelov@gmail.com>
2021-09-25 13:58:45 +1000

    natspec auto-completion in comments

---
+++ src/test/kotlin/me/serce/solidity/lang/completion/SolNatSpecCompletionTest.kt
@@ -0,0 +1,15 @@
+package me.serce.solidity.lang.completion
+
+class SolNatSpecCompletionTest : SolCompletionTestBase() {
+  fun testNatSpecCommentCompletion() = checkCompletion(hashSetOf("author", "title", "return", "notice"), """
+        /// /*caret*/
+        contract A {}
+  """, strict = false)
+
+  fun testNatSpecTagCompletion() = checkCompletion(hashSetOf("@author"), """
+        /**
+         * @au/*caret*/
+         */ 
+        contract A {}
+  """, strict = true)
+}
