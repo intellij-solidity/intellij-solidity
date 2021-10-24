ec48d2cc26143f50756c41b5c5057276dfdce1b5
ualtinok <ufukaltinok@gmail.com>
2021-10-24 19:53:35 +0800

    add gwei and remove finney and szabo (intellij-solidity#261) (#262)

--- src/test/resources/fixtures/parser/solidity_official_tests/shouldSucceed/literalConstantsWithEtherSubdenominations.sol
+++ src/test/resources/fixtures/parser/solidity_official_tests/shouldSucceed/literalConstantsWithEtherSubdenominations.sol
@@ -2,12 +2,10 @@ contract c {
   function c ()
   {
      a = 1 wei;
-     b = 2 szabo;
-     c = 3 finney;
-     b = 4 ether;
+     b = 2 gwei;
+     c = 4 ether;
   }
   uint256 a;
   uint256 b;
   uint256 c;
-  uint256 d;
 }
