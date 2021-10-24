b2cbf458b295ca72af1ed13fd60b8cbfa65af638
Sergey Tselovalnikov <sergeicelov@gmail.com>
2021-10-24 23:10:16 +1100

    reintroduce deprecated szabo & finney

--- src/test/resources/fixtures/parser/solidity_official_tests/shouldSucceed/literalConstantsWithEtherSubdenominations.sol
+++ src/test/resources/fixtures/parser/solidity_official_tests/shouldSucceed/literalConstantsWithEtherSubdenominations.sol
@@ -3,9 +3,13 @@ contract c {
   {
      a = 1 wei;
      b = 2 gwei;
-     c = 4 ether;
+     c = 3 szabo;
+     d = 4 finney;
+     e = 5 ether;
   }
   uint256 a;
   uint256 b;
   uint256 c;
+  uint256 d;
+  uint256 e;
 }
