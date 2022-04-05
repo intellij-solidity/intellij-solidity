87160b5a3b1c259452ee7bd874ac3a5dd4bfcfd0
Sergey Tselovalnikov <sergeicelov@gmail.com>
2022-04-06 00:29:02 +1000

    grammar: allow builtins as yul identifier paths in antlr grammar (#292)

--- src/test/resources/fixtures/parser/assembly.sol
+++ src/test/resources/fixtures/parser/assembly.sol
@@ -140,3 +140,13 @@ contract Foo {
         }
     }
 }
+
+contract C {
+    function f() public pure {
+        function() external g;
+        assembly {
+            g.address := 0x42
+            g.selector := 0x23
+        }
+    }
+}
