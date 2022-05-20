776f912c8243abd2299228d530d3d69b26d1c8bd
Andrew Potter <apottere@gmail.com>
2022-05-20 16:43:59 -0400

    Improve highlighting

--- src/main/kotlin/me/serce/solidity/ide/annotation/annotator.kt
+++ src/main/kotlin/me/serce/solidity/ide/annotation/annotator.kt
@@ -3,33 +3,85 @@ package me.serce.solidity.ide.annotation
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.refactoring.suggested.endOffset
 import me.serce.solidity.ide.colors.SolColor
+import me.serce.solidity.ide.hints.startOffset
 import me.serce.solidity.lang.psi.*
 import me.serce.solidity.lang.psi.impl.SolErrorDefMixin
 
 class SolidityAnnotator : Annotator {
   override fun annotate(element: PsiElement, holder: AnnotationHolder) {
     if (element is SolElement) {
-      val highlight = highlight(element)
-      if (highlight != null) {
-        val (partToHighlight, color) = highlight
-        holder.newAnnotation(HighlightSeverity.INFORMATION, "")
-          .range(partToHighlight)
-          .textAttributes(color.textAttributesKey)
-          .create()
-      }
+      highlight(element, holder)
     }
   }
 
-  private fun highlight(element: SolElement): Pair<PsiElement, SolColor>? {
-    return when (element) {
-      is SolNumberType -> element to SolColor.KEYWORD
-      is SolElementaryTypeName -> element to SolColor.KEYWORD
-      is SolErrorDefMixin -> element.identifier to SolColor.KEYWORD
-      is SolRevertStatement -> element.firstChild to SolColor.KEYWORD
-      is SolUserDefinedTypeName -> element to SolColor.CONTRACT_REFERENCE
-      else -> null
+  private fun highlight(element: SolElement, holder: AnnotationHolder) {
+    when (element) {
+      is SolNumberType -> applyColor(holder, element, SolColor.TYPE)
+      is SolElementaryTypeName -> applyColor(holder, element, SolColor.TYPE)
+      is SolErrorDefMixin -> {
+        applyColor(holder, element.identifier, SolColor.KEYWORD)
+        element.nameIdentifier?.let { applyColor(holder, it, SolColor.ERROR_NAME) }
+      }
+      is SolHexLiteral -> {
+        if ((element.endOffset - element.startOffset) > 4) {
+          applyColor(holder, TextRange(element.startOffset, element.startOffset + 3), SolColor.KEYWORD)
+          applyColor(holder, TextRange(element.startOffset + 3, element.endOffset), SolColor.STRING)
+        } else {
+          applyColor(holder, element, SolColor.STRING)
+        }
+      }
+      is SolRevertStatement -> applyColor(holder, element.firstChild, SolColor.KEYWORD)
+      is SolOverrideSpecifier -> {
+        if ((element.endOffset - element.startOffset) > 8) {
+          applyColor(holder, TextRange(element.startOffset, element.startOffset + 8), SolColor.KEYWORD)
+        }
+      }
+      is SolContractDefinition -> element.identifier?.let { applyColor(holder, it, SolColor.CONTRACT_NAME) }
+      is SolStructDefinition -> element.identifier?.let { applyColor(holder, it, SolColor.STRUCT_NAME) }
+      is SolEventDefinition -> element.identifier?.let { applyColor(holder, it, SolColor.EVENT_NAME) }
+      is SolConstantVariableDeclaration -> applyColor(holder, element.identifier, SolColor.CONSTANT_NAME)
+      is SolStateVariableDeclaration -> {
+        if (element.mutationModifier?.textMatches("constant") == true) {
+          applyColor(holder, element.identifier, SolColor.STATE_VARIABLE_NAME)
+        } else {
+          applyColor(holder, element.identifier, SolColor.STATE_VARIABLE_NAME)
+        }
+      }
+      is SolFunctionDefinition -> element.identifier?.let { applyColor(holder, it, SolColor.FUNCTION_DECLARATION) }
+      is SolModifierDefinition -> element.identifier?.let { applyColor(holder, it, SolColor.FUNCTION_DECLARATION) }
+      is SolModifierInvocation -> applyColor(holder, element.varLiteral.identifier, SolColor.FUNCTION_CALL)
+
+      // TODO: the following destroys IDE performance, but does provide correct contextual highlighting for
+      // TODO: types and function calls... probable smoking gun is `getReference()`?
+      // TODO: Default to previous behavior until performance issue is fixed
+      is SolUserDefinedTypeName -> applyColor(holder, element, SolColor.CONTRACT_REFERENCE)
+//      is SolUserDefinedTypeName -> when(element.reference?.resolve()) {
+//        is SolContractDefinition -> applyColor(holder, element, SolColor.CONTRACT_NAME)
+//        is SolStructDefinition -> applyColor(holder, element, SolColor.STRUCT_NAME)
+//      }
+//      is SolFunctionCallElement -> when(element.reference?.resolve()) {
+//        is SolFunctionDefinition -> applyColor(holder, element.referenceNameElement, SolColor.FUNCTION_CALL)
+//        is SolErrorDefinition -> applyColor(holder, element.referenceNameElement, SolColor.ERROR_NAME)
+//        is SolEventDefinition -> applyColor(holder, element.referenceNameElement, SolColor.EVENT_NAME)
+//      }
     }
+  }
+
+  private fun applyColor(holder: AnnotationHolder, element: PsiElement, color: SolColor) {
+    holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+      .range(element)
+      .textAttributes(color.textAttributesKey)
+      .create()
+  }
+
+  private fun applyColor(holder: AnnotationHolder, range: TextRange, color: SolColor) {
+    holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+      .range(range)
+      .textAttributes(color.textAttributesKey)
+      .create()
   }
 }
--- src/main/kotlin/me/serce/solidity/ide/colors/SolColor.kt
+++ src/main/kotlin/me/serce/solidity/ide/colors/SolColor.kt
@@ -8,6 +8,18 @@ enum class SolColor(humanName: String, default: TextAttributesKey) {
   LINE_COMMENT("Comment", Defaults.LINE_COMMENT),
   NAT_SPEC_TAG("NatSpecTag", Defaults.DOC_COMMENT_TAG),
 
+  CONTRACT_NAME("Contract name", Defaults.CLASS_NAME),
+  STRUCT_NAME("Struct name", Defaults.CLASS_NAME),
+  ERROR_NAME("Error name", Defaults.CLASS_NAME),
+  EVENT_NAME("Event name", Defaults.CLASS_NAME),
+
+  CONSTANT_NAME("Constant name", Defaults.GLOBAL_VARIABLE),
+  CONSTANT_STATE_VARIABLE_NAME("Constant state variable name", Defaults.STATIC_FIELD),
+  STATE_VARIABLE_NAME("State variable name", Defaults.INSTANCE_FIELD),
+
+  FUNCTION_DECLARATION("Function declaration", Defaults.FUNCTION_DECLARATION),
+  FUNCTION_CALL("Function call", Defaults.FUNCTION_CALL),
+
   BRACES("Braces", Defaults.BRACES),
   BRACKETS("Brackets", Defaults.BRACKETS),
   PARENTHESES("Parentheses", Defaults.PARENTHESES),
@@ -16,6 +28,7 @@ enum class SolColor(humanName: String, default: TextAttributesKey) {
   NUMBER("Number", Defaults.NUMBER),
   STRING("String", Defaults.STRING),
   KEYWORD("Keyword", Defaults.KEYWORD),
+  TYPE("Type", Defaults.KEYWORD),
 
   OPERATION_SIGN("Operation signs", Defaults.OPERATION_SIGN),
   CONTRACT_REFERENCE("Contract reference", Defaults.CLASS_REFERENCE),
