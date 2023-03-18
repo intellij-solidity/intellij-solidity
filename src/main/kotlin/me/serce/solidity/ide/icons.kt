19dcaf7beacc1c69252f194d059c45458068dfda
Sergey Tselovalnikov <sergey@serce.me>
2023-03-18 23:23:10 +1100

    bump minimum IntelliJ version to 2022.2.5

--- src/main/kotlin/me/serce/solidity/ide/icons.kt
+++ src/main/kotlin/me/serce/solidity/ide/icons.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object SolidityIcons {
-  val FILE_ICON: Icon = IconLoader.getIcon("/icons/sol-file.png")
+  val FILE_ICON: Icon = IconLoader.getIcon("/icons/sol-file.png", SolidityIcons::class.java)
 
   val ENUM: Icon = IconLoader.getIcon("/icons/sol-enum.png", SolidityIcons::class.java)
   val EVENT: Icon = IconLoader.getIcon("/icons/sol-enum.png", SolidityIcons::class.java)
