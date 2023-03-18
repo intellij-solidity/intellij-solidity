521b1ce45543e5ab064e9a4e90bd29e285209f6b
Sergey Tselovalnikov <sergey@serce.me>
2023-03-18 22:30:32 +1100

    update deprecated methods

--- src/main/kotlin/me/serce/solidity/ide/icons.kt
+++ src/main/kotlin/me/serce/solidity/ide/icons.kt
@@ -6,11 +6,11 @@ import javax.swing.Icon
 object SolidityIcons {
   val FILE_ICON: Icon = IconLoader.getIcon("/icons/sol-file.png")
 
-  val ENUM: Icon = IconLoader.getIcon("/icons/sol-enum.png")
-  val EVENT: Icon = IconLoader.getIcon("/icons/sol-enum.png")
-  val ERROR: Icon = IconLoader.getIcon("/icons/sol-enum.png")
-  val CONTRACT: Icon = IconLoader.getIcon("/icons/sol-contract.png")
-  val STRUCT: Icon = IconLoader.getIcon("/icons/sol-contract.png")
-  val FUNCTION: Icon = IconLoader.getIcon("/icons/sol-method.png")
-  val STATE_VAR: Icon = IconLoader.getIcon("/icons/sol-state-var.png")
+  val ENUM: Icon = IconLoader.getIcon("/icons/sol-enum.png", SolidityIcons::class.java)
+  val EVENT: Icon = IconLoader.getIcon("/icons/sol-enum.png", SolidityIcons::class.java)
+  val ERROR: Icon = IconLoader.getIcon("/icons/sol-enum.png", SolidityIcons::class.java)
+  val CONTRACT: Icon = IconLoader.getIcon("/icons/sol-contract.png", SolidityIcons::class.java)
+  val STRUCT: Icon = IconLoader.getIcon("/icons/sol-contract.png", SolidityIcons::class.java)
+  val FUNCTION: Icon = IconLoader.getIcon("/icons/sol-method.png", SolidityIcons::class.java)
+  val STATE_VAR: Icon = IconLoader.getIcon("/icons/sol-state-var.png", SolidityIcons::class.java)
 }
