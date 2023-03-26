e3141657b3baae36f552b2ee9f059c691ceb0471
Sergey Tselovalnikov <sergey@serce.me>
2023-03-26 20:37:36 +1100

    remove solidity runner (#353)

--- src/main/kotlin/me/serce/solidity/ide/run/SolidityRunConfigurationExtensionManager.kt
+++
@@ -1,7 +0,0 @@
-package me.serce.solidity.ide.run
-
-import com.intellij.execution.RunConfigurationExtension
-import com.intellij.execution.configuration.RunConfigurationExtensionsManager
-import com.intellij.execution.configurations.RunConfigurationBase
-
-class SolidityRunConfigurationExtensionManager : RunConfigurationExtensionsManager<RunConfigurationBase<*>, RunConfigurationExtension>(RunConfigurationExtension.EP_NAME)
