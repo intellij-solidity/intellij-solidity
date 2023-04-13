3951684361469dcb28667e5ef367eb4512d57ec0
Mikhail Bolotov <mikhail.bolotov@gmail.com>
2023-04-13 09:51:50 +0300

    feature: show code docs on hovering. support for inherited docs

--- src/test/kotlin/me/serce/solidity/ide/hints/SolDocumentationProviderTest.kt
+++ src/test/kotlin/me/serce/solidity/ide/hints/SolDocumentationProviderTest.kt
@@ -20,9 +20,7 @@ class SolDocumentationProviderTest : SolTestBase() {
                  return 1;
               }
           }
-      """, """<div class='definition'><pre><b style='color:rgb(0,0,128)'>function</b> withDoc(<b style='color:rgb(0,0,128)'>int</b> myInti, <b style='color:rgb(0,0,128)'><b style='color:rgb(0,0,128)'>bool</b></b> aBool)  <b style='color:rgb(0,0,128)'>returns</b> (<b style='color:rgb(0,0,128)'>uint</b>)</pre></div><div class='content'>
-                my docs
-              </div>""")
+      """, """<div class='definition'><pre><b style='color:rgb(0,0,128)'>function</b> withDoc(<b style='color:rgb(0,0,128)'>int</b> myInti, <b style='color:rgb(0,0,128)'><b style='color:rgb(0,0,128)'>bool</b></b> aBool)  <b style='color:rgb(0,0,128)'>returns</b> (<b style='color:rgb(0,0,128)'>uint</b>)</pre></div><div class='content'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;my&nbsp;docs</div>""")
 
   fun testDocsForBuiltinFunction() = checkByText("""
             contract A {
@@ -30,10 +28,7 @@ class SolDocumentationProviderTest : SolTestBase() {
                     abi.encode/*caret*/();
                 }
             }
-        """,      """<div class='definition'><pre><b style='color:rgb(0,0,128)'>function</b> encode(<b style='color:rgb(0,0,128)'>data</b>)    <b style='color:rgb(0,0,128)'>returns</b> (<b style='color:rgb(0,0,128)'>bytes</b> <b style='color:rgb(0,0,128)'>memory</b>)</pre></div><div class='content'>
-ABI-encodes the given arguments
-                   
-  							</div>""".trimMargin())
+        """, """<div class='definition'><pre><b style='color:rgb(0,0,128)'>function</b> encode(<b style='color:rgb(0,0,128)'>data</b>)  <b style='color:rgb(0,0,128)'>returns</b> (<b style='color:rgb(0,0,128)'>bytes</b> <b style='color:rgb(0,0,128)'>memory</b>)</pre></div><div class='content'>ABI-encodes&nbsp;the&nbsp;given&nbsp;arguments</div>""")
 
   fun testTripleSlashDocs() = checkByText("""
           contract A {
@@ -46,8 +41,7 @@ ABI-encodes the given arguments
                  return 1;
               }
           }
-      """, """<div class='definition'><pre><b style='color:rgb(0,0,128)'>function</b> withDoc(<b style='color:rgb(0,0,128)'>int</b> myInti, <b style='color:rgb(0,0,128)'><b style='color:rgb(0,0,128)'>bool</b></b> aBool)  <b style='color:rgb(0,0,128)'>returns</b> (<b style='color:rgb(0,0,128)'>uint</b>)</pre></div><div class='content'> <br/><span class='grayed'>notice:</span> my docs
-              </div>""")
+      """, """<div class='definition'><pre><b style='color:rgb(0,0,128)'>function</b> withDoc(<b style='color:rgb(0,0,128)'>int</b> myInti, <b style='color:rgb(0,0,128)'><b style='color:rgb(0,0,128)'>bool</b></b> aBool)  <b style='color:rgb(0,0,128)'>returns</b> (<b style='color:rgb(0,0,128)'>uint</b>)</pre></div><div class='content'>&nbsp;<span&nbsp;class='grayed'>notice:</span>&nbsp;my&nbsp;docs</div>""")
 
   private fun checkByText(@Language("Solidity") code: String, @Language("HTML") expectedDoc: String) {
     myFixture.configureByText("main.sol", code.replace("/*caret*/", "<caret>"))
