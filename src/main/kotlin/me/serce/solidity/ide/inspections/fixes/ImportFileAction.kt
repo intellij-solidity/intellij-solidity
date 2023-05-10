7762eaea1d599d0a099523dac0994678b652881c
Mikhail Bolotov <mikhail.bolotov@gmail.com>
2023-05-10 17:17:33 +0300

    do not use backslash in import path (affects Windows only). fixes #390

--- src/main/kotlin/me/serce/solidity/ide/inspections/fixes/ImportFileAction.kt
+++ src/main/kotlin/me/serce/solidity/ide/inspections/fixes/ImportFileAction.kt
@@ -19,7 +19,6 @@ import me.serce.solidity.lang.psi.SolPragmaDirective
 import me.serce.solidity.lang.psi.SolPsiFactory
 import me.serce.solidity.nullIfError
 import java.awt.BorderLayout
-import java.io.File
 import java.nio.file.Paths
 import javax.swing.Icon
 import javax.swing.JPanel
@@ -127,7 +126,7 @@ class ImportFileAction(
 
     fun buildImportPath(source: VirtualFile, destination: VirtualFile): String {
       return Paths.get(source.path).parent.relativize(Paths.get(destination.path)).toString().let {
-        val separator = File.separator
+        val separator = "/"
         when {
             it.contains("node_modules$separator") -> {
               val idx = it.indexOf("node_modules$separator")
@@ -145,7 +144,7 @@ class ImportFileAction(
             !it.startsWith(".") -> ".$separator$it"
             else -> it
         }
-      }
+      }.replace("\\", "/")
     }
   }
 }
