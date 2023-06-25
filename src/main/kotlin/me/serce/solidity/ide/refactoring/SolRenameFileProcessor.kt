cbd3e14b1e6592373e41a0a1cad0addae1847dc1
Mikhail Bolotov <mikhail.bolotov@gmail.com>
2023-06-25 05:45:40 +0300

    support for move refactoring (#397)

---
+++ src/main/kotlin/me/serce/solidity/ide/refactoring/SolRenameFileProcessor.kt
@@ -0,0 +1,24 @@
+package me.serce.solidity.ide.refactoring
+
+import com.intellij.openapi.ui.MessageDialogBuilder
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.SearchScope
+import com.intellij.refactoring.rename.RenamePsiFileProcessor
+import me.serce.solidity.lang.core.SolidityFile
+import me.serce.solidity.lang.psi.SolContractOrLibElement
+
+class SolRenameFileProcessor : RenamePsiFileProcessor() {
+  override fun prepareRenaming(element: PsiElement, newName: String, allRenames: MutableMap<PsiElement, String>, scope: SearchScope) {
+    (element as? SolidityFile)?.let { file ->
+      val contract = file.children.find { it is SolContractOrLibElement && it.name == file.virtualFile.nameWithoutExtension && it.name != newName } as? SolContractOrLibElement
+      if (contract != null) {
+        val description = contract.contractType.docName
+        val renameContract = /*!RefactoringSettings.getInstance().ASK_FOR_RENAME_DECLARATION_WHEN_RENAME_FILE ||*/ MessageDialogBuilder.yesNo("Rename $description", "Do you also want to rename the $description").ask(element.getProject())
+        if (renameContract) {
+          allRenames[contract] = newName
+        }
+      }
+    }
+    super.prepareRenaming(element, newName, allRenames, scope)
+  }
+}
--- src/test/resources/fixtures/refactoring/rename/imports/nested/ImportingFile.sol
+++ src/test/resources/fixtures/refactoring/rename/imports/nested/ImportingFile.sol
@@ -1,5 +1,5 @@
 import "../Lab.sol";
 
 contract IUseLab {
-  Lab lab;
+  Lab1 lab;
 }
