d9328f7152c8ca30cee5bdef42be662e906fa5b5
Mikhail Bolotov <mikhail.bolotov@gmail.com>
2024-05-13 19:39:28 +0300

    inline rename

--- src/main/kotlin/me/serce/solidity/ide/refactoring/RenameContractProcessor.kt
+++ src/main/kotlin/me/serce/solidity/ide/refactoring/RenameContractProcessor.kt
@@ -1,8 +1,10 @@
 package me.serce.solidity.ide.refactoring
 
+import com.intellij.openapi.ui.MessageDialogBuilder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.SearchScope
 import com.intellij.refactoring.rename.RenamePsiElementProcessor
+import me.serce.solidity.lang.SolidityFileType
 import me.serce.solidity.lang.psi.SolContractDefinition
 
 class RenameContractProcessor : RenamePsiElementProcessor() {
@@ -11,7 +13,17 @@ class RenameContractProcessor : RenamePsiElementProcessor() {
   }
 
   override fun prepareRenaming(element: PsiElement, newName: String, allRenames: MutableMap<PsiElement, String>, scope: SearchScope) {
-    (element as SolContractDefinition).functionDefinitionList
+    val contract = element as SolContractDefinition
+    val file = element.containingFile
+    if (contract.name == file.virtualFile.nameWithoutExtension) {
+      if (newName != "") {
+        val renameContract = !allRenames.keys.contains(file) && MessageDialogBuilder.yesNo("Rename file", "Do you also want to rename the ${file.name} file?").ask(element.getProject())
+        if (renameContract) {
+          allRenames[file] = "$newName.${SolidityFileType.defaultExtension}"
+        }
+      }
+    }
+    contract.functionDefinitionList
       .filter { it.isConstructor }
       .forEach { allRenames[it] = newName }
   }
