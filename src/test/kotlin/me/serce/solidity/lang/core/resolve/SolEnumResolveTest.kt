d3223be2fef6f687883d35187f01b4d5bd853102
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-10-11 09:35:51 +0200

    test: imported enum at file level

--- src/test/kotlin/me/serce/solidity/lang/core/resolve/SolEnumResolveTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/core/resolve/SolEnumResolveTest.kt
@@ -101,4 +101,55 @@ class SolEnumResolveTest : SolResolveTestBase() {
             }
         }
   """)
+
+  fun testResolveImportedEnum() = testResolveBetweenFiles(
+    InlineFile(
+      code = """
+          pragma solidity ^0.8.26;
+          
+           enum B { A1, A2 }
+              //x
+      """,
+      name = "a.sol"
+    ),
+    InlineFile(
+      """
+          pragma solidity ^0.8.26;      
+                
+          import {B} from "./a.sol";
+                //^
+          contract b {
+              function f() {
+                  B.A2;
+              }
+          }
+    """
+    )
+  )
+
+  fun testResolveImportedEnum2() = testResolveBetweenFiles(
+    InlineFile(
+      code = """
+          pragma solidity ^0.8.26;
+          
+           enum B { A1, A2 }
+              //x
+      """,
+      name = "a.sol"
+    ),
+    InlineFile(
+      """
+          pragma solidity ^0.8.26;      
+                
+          import {B} from "./a.sol";
+                
+          contract b {
+              function f() {
+                  B.A2;
+                //^
+              }
+          }
+    """
+    )
+  )
 }
