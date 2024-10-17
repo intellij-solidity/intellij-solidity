a1f554dcec89a6ac2fdc9af9762314c681825ef1
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-10-17 18:05:31 +0200

    test: add custom type resolve

--- src/test/kotlin/me/serce/solidity/lang/core/resolve/SolUserDefinedValueTypeResolveTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/core/resolve/SolUserDefinedValueTypeResolveTest.kt
@@ -20,4 +20,78 @@ class SolUserDefinedValueTypeResolveTest : SolResolveTestBase() {
                                             //^
         }
   """)
+
+  fun testUserDefinedValueTypeResolveAlias() = testResolveBetweenFiles(
+    InlineFile(
+      code = """
+         pragma solidity ^0.8.26;
+         
+         type Decimal18 is uint256;
+                //x
+    """,
+      name = "a.sol"
+    ),
+    InlineFile(
+      """
+        pragma solidity ^0.8.26;
+        
+        import {Decimal18 as customType} from "./a.sol";
+
+        contract C {
+            function transfer(address to, customType value) external;
+                                            //^
+       }
+  """
+    )
+  )
+
+  fun testUserDefinedValueTypeResolveAlias2() = testResolveBetweenFiles(
+    InlineFile(
+      code = """
+         pragma solidity ^0.8.26;
+         
+         type Decimal18 is uint256;
+                //x
+    """,
+      name = "a.sol"
+    ),
+    InlineFile(
+      """
+        pragma solidity ^0.8.26;
+        
+        import {Decimal18 as customType} from "./a.sol";
+                                //^
+
+        contract C {
+            function transfer(address to, customType value) external;
+       }
+  """
+    )
+  )
+
+  fun testUserDefinedValueTypeResolveAliasInInterface() = testResolveBetweenFiles(
+    InlineFile(
+      code = """
+         pragma solidity ^0.8.26;
+         
+         interface InterfaceI {
+           type Decimal18 is uint256;
+                  //x
+         }
+    """,
+      name = "a.sol"
+    ),
+    InlineFile(
+      """
+        pragma solidity ^0.8.26;
+        
+        import {InterfaceI as Types} from "./a.sol";
+
+        contract C {
+            function transfer(address to, Types.Decimal18 value) external;
+                                                //^
+       }
+  """
+    )
+  )
 }
