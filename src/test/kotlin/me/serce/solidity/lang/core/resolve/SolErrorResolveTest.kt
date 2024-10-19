436b9863516c4d16a4274a75e0025bd12874e441
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-10-19 16:24:39 +0200

    test: error resolve

--- src/test/kotlin/me/serce/solidity/lang/core/resolve/SolErrorResolveTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/core/resolve/SolErrorResolveTest.kt
@@ -133,6 +133,30 @@ class SolErrorResolveTest : SolResolveTestBase() {
   """)
   )
 
+  fun testResolveErrorFromAlias2() = testResolveBetweenFiles(
+    InlineFile(
+      code = """
+         pragma solidity ^0.8.26;
+         
+          error Closed();
+                 //x
+    """,
+      name = "a.sol"
+    ),
+    InlineFile("""
+        pragma solidity ^0.8.26;
+        
+        import {Closed as ErrorClosed} from "./a.sol";
+                           //^
+
+        contract C {
+            function f() public {
+               revert ErrorClosed();
+            }
+       }
+  """)
+  )
+
   fun testResolveErrorFromAliasInInterface() = testResolveBetweenFiles(
     InlineFile(
       code = """
