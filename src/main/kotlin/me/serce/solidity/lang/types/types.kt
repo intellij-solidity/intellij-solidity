45c97eb5eec7641e56bebd97b56ee5923551e72e
passabilities.eth <noah@passabilities.dev>
2025-06-13 12:06:35 -0500

    revert change

--- src/main/kotlin/me/serce/solidity/lang/types/types.kt
+++ src/main/kotlin/me/serce/solidity/lang/types/types.kt
@@ -225,6 +225,7 @@ data class SolContract(val ref: SolContractDefinition, val builtin: Boolean = fa
       .flatMap { it.reference?.multiResolve() ?: emptyList() }
       .filterIsInstance<SolContractDefinition>()
       .map { SolContract(it) }
+      .reversed()
   }
 
   override fun isAssignableFrom(other: SolType): Boolean =
