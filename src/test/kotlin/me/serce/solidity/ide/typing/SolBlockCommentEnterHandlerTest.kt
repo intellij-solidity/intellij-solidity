c1c9f189443e3b5fdf95f7db61c4820865c9121e
Sergey Tselovalnikov <sergey@serce.me>
2025-08-08 21:44:39 +1000

    fix #494: multiline comment auto-completion

---
+++ src/test/kotlin/me/serce/solidity/ide/typing/SolBlockCommentEnterHandlerTest.kt
@@ -0,0 +1,60 @@
+package me.serce.solidity.ide.typing
+
+import me.serce.solidity.utils.SolTestBase
+
+class SolBlockCommentEnterHandlerTest : SolTestBase() {
+  fun testBlockCommentContinuation() = checkByText(
+    "/*<caret>",
+    """
+      /*
+       * <caret>
+       */
+    """.trimIndent()
+  ) {
+    myFixture.type('\n')
+  }
+
+  fun testBlockCommentContinuationWithDoubleStars() = checkByText(
+    "/**<caret>",
+    """
+      /**
+       * <caret>
+       */
+    """.trimIndent()
+  ) {
+    myFixture.type('\n')
+  }
+
+  fun testBlockCommentEnterWithinTheComment() = checkByText(
+    """
+      /*
+       * <caret>
+       */
+    """.trimIndent(),
+    """
+      /*
+       *
+       * <caret>
+       */
+    """.trimIndent()
+  ) {
+    myFixture.type('\n')
+  }
+
+  fun testBlockCommentContinuationWithIndent() = checkByText(
+    """
+      contract C {
+        /*<caret>
+      }
+    """.trimIndent(),
+    """
+      contract C {
+        /*
+         * <caret>
+         */
+      }
+    """.trimIndent()
+  ) {
+    myFixture.type('\n')
+  }
+}
