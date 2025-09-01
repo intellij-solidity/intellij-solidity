63da9fba53f438b997878fe34c8f856ff88f51b5
Paulalex85 <tessier.paulalexandre@orange.fr>
2025-09-01 18:19:54 +0200

    fix: remove annotations

--- src/main/kotlin/me/serce/solidity/settings/SoliditySettingsState.kt
+++ src/main/kotlin/me/serce/solidity/settings/SoliditySettingsState.kt
@@ -1,24 +1,18 @@
 package me.serce.solidity.settings
 
 import com.intellij.openapi.components.BaseState
-import com.intellij.openapi.components.Service
-import org.jetbrains.annotations.ApiStatus
 
-@ApiStatus.Internal
 enum class ConfigurationMode {
   AUTOMATIC,
   MANUAL
 }
 
-@ApiStatus.Internal
 enum class FormatterType {
   INTELLIJ_SOLIDITY,
   FOUNDRY,
   PRETTIER
 }
 
-@Service
-@ApiStatus.Internal
 class SoliditySettingsState : BaseState() {
   var formatterType by enum(FormatterType.INTELLIJ_SOLIDITY)
   var configurationMode by enum(ConfigurationMode.AUTOMATIC)
