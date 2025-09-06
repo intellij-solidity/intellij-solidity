8b211774fc71c532a303c2a9ab35fa5756d50c89
Sergey Tselovalnikov <sergey@serce.me>
2025-09-07 09:48:13 +1000

    formatter: ensure the restriction doesn't affect non-sol files

--- src/main/kotlin/me/serce/solidity/ide/formatting/SolidityFormattingRestriction.kt
+++ src/main/kotlin/me/serce/solidity/ide/formatting/SolidityFormattingRestriction.kt
@@ -2,12 +2,19 @@ package me.serce.solidity.ide.formatting
 
 import com.intellij.lang.LanguageFormattingRestriction
 import com.intellij.psi.PsiElement
+import me.serce.solidity.lang.core.SolidityFile
 import me.serce.solidity.settings.FormatterType
 import me.serce.solidity.settings.SoliditySettings
 
 class SolidityFormattingRestriction : LanguageFormattingRestriction {
 
   override fun isFormatterAllowed(element: PsiElement): Boolean {
+    // ensure only Solidity files are restricted by the formatter settings
+    val file = element.containingFile
+    if (file !is SolidityFile) {
+      return true
+    }
+
     val settings = SoliditySettings.getInstance(element.project)
     return settings.formatterType == FormatterType.INTELLIJ_SOLIDITY
   }
---
+++ src/test/kotlin/me/serce/solidity/ide/formatting/SolidityFormattingRestrictionTest.kt
@@ -0,0 +1,42 @@
+package me.serce.solidity.ide.formatting
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import me.serce.solidity.settings.FormatterType
+import me.serce.solidity.settings.SoliditySettings
+
+class SolidityFormattingRestrictionTest : BasePlatformTestCase() {
+
+  fun testNonSolidityFilesAreAlwaysAllowed() {
+    val psi = myFixture.configureByText("A.txt", "plain text")
+    SoliditySettings.getInstance(project).apply {
+      // any value here shouldn't change the behaviour
+      formatterType = FormatterType.DISABLED
+    }
+
+    val restriction = SolidityFormattingRestriction()
+    assertTrue(restriction.isFormatterAllowed(psi))
+  }
+
+  fun testSolidityAllowsIdeFormatterWhenIntellijFormatterSelected() {
+    val psi = myFixture.configureByText("B.sol", "contract B { }")
+    SoliditySettings.getInstance(project).apply {
+      formatterType = FormatterType.INTELLIJ_SOLIDITY
+    }
+
+    val restriction = SolidityFormattingRestriction()
+    assertTrue(restriction.isFormatterAllowed(psi))
+  }
+
+  fun testSolidityBlocksIdeFormatterWhenFoundryOrDisabledSelected() {
+    val psi = myFixture.configureByText("C.sol", "contract C { }")
+    val settings = SoliditySettings.getInstance(project)
+
+    val restriction = SolidityFormattingRestriction()
+
+    settings.formatterType = FormatterType.FOUNDRY
+    assertFalse(restriction.isFormatterAllowed(psi))
+
+    settings.formatterType = FormatterType.DISABLED
+    assertFalse(restriction.isFormatterAllowed(psi))
+  }
+}
