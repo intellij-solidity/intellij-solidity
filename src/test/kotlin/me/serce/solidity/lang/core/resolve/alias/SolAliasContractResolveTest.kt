07f17ee03d4f82178b0c2c5bd5c566dee374b03b
Paulalex85 <tessier.paulalexandre@orange.fr>
2025-09-09 18:39:25 +0200

    test: add failing cases

--- src/test/kotlin/me/serce/solidity/lang/core/resolve/alias/SolAliasContractResolveTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/core/resolve/alias/SolAliasContractResolveTest.kt
@@ -128,4 +128,50 @@ class SolAliasContractResolveTest : SolResolveTestBase() {
       """)
     )
   }
+
+  fun testResolveContractFromAliasWithInheritance() {
+    InlineFile(
+      code = """
+        pragma solidity ^0.8.0;
+
+        import {Types as Constants} from "./types.sol";
+        
+        contract Parent {
+            function foo() public pure returns (uint256) {
+                return 42;
+            }
+        }
+    """,
+      name = "parent.sol"
+    )
+
+    testResolveBetweenFiles(
+      InlineFile(
+        code = """
+        pragma solidity ^0.8.0;
+
+        library Types {
+                //x
+            uint256 public constant VALUE = 256;
+        }
+    """,
+        name = "types.sol"
+      ),
+      InlineFile(
+        code = """
+        pragma solidity ^0.8.0;
+
+        import "./parent.sol";
+        
+        contract Child is Parent {
+            function foo2() public pure returns (uint256) {
+                return Constants.VALUE;
+                          //^
+            }
+        }
+    """,
+        name = "parent.sol"
+      )
+    )
+  }
 }
--- src/test/kotlin/me/serce/solidity/lang/core/resolve/alias/SolAliasVarResolveTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/core/resolve/alias/SolAliasVarResolveTest.kt
@@ -139,4 +139,50 @@ class SolAliasVarResolveTest : SolResolveTestBase() {
   """
     )
   )
+
+  fun testResolveVarFromAliasWithInheritance() {
+    InlineFile(
+      code = """
+        pragma solidity ^0.8.0;
+
+        import {Types as Constants} from "./types.sol";
+        
+        contract Parent {
+            function foo() public pure returns (uint256) {
+                return 42;
+            }
+        }
+    """,
+      name = "parent.sol"
+    )
+
+    testResolveBetweenFiles(
+      InlineFile(
+        code = """
+        pragma solidity ^0.8.0;
+
+        library Types {
+            uint256 public constant VALUE = 256;
+                                    //^
+        }
+    """,
+        name = "types.sol"
+      ),
+      InlineFile(
+        code = """
+        pragma solidity ^0.8.0;
+
+        import "./parent.sol";
+        
+        contract Child is Parent {
+            function foo2() public pure returns (uint256) {
+                return Constants.VALUE;
+                                //^
+            }
+        }
+    """,
+        name = "parent.sol"
+      )
+    )
+  }
 }
