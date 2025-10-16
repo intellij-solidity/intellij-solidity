6dd73e83772e82725f00cf746a9eca0c664b2195
Paulalex85 <tessier.paulalexandre@pm.me>
2025-10-16 18:23:56 +0200

    test: more cases

--- src/test/kotlin/me/serce/solidity/lang/completion/SolImportCompletionTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/completion/SolImportCompletionTest.kt
@@ -5,13 +5,35 @@ class SolImportCompletionTest : SolCompletionTestBase() {
         InlineFile(
             code = """contract test {}""", name = "test.sol"
         )
+        checkCompletion(
+            hashSetOf("./test.sol"), """
+        import {test} from "./te/*caret*/
+  """
+        )
+    }
+
+    fun testImportCompletionWithEndQuote() {
+        InlineFile(
+            code = """contract test {}""", name = "test.sol"
+        )
+        checkCompletion(
+            hashSetOf("./test.sol"), """
+        import {test} from "./te/*caret*/"
+  """
+        )
+    }
+
+    fun testImportCompletionAfterCompletion() {
+        InlineFile(
+            code = """contract test {}""", name = "test.sol"
+        )
 
         checkResultAfterCompletion(
             """import {test} from "./te/*caret*/ """, """import {test} from "./test.sol";"""
         )
     }
 
-    fun testImportCompletionWithAlreadyEndQuote() {
+    fun testImportCompletionAfterCompletionWithEndQuote() {
         InlineFile(
             code = """contract test {}""", name = "test.sol"
         )
