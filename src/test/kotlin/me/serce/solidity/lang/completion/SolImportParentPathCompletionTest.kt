0de42429ae524ee9e388dc8231bee0336f61773e
Paulalex85 <tessier.paulalexandre@pm.me>
2025-11-10 18:05:14 +0100

    refactor: completion parent dir tests

--- src/test/kotlin/me/serce/solidity/lang/completion/SolImportParentPathCompletionTest.kt
+++ src/test/kotlin/me/serce/solidity/lang/completion/SolImportParentPathCompletionTest.kt
@@ -1,77 +1,59 @@
 package me.serce.solidity.lang.completion
 
 class SolImportParentPathCompletionTest : SolCompletionTestBase() {
-    fun testImportCompletionPreviousFolderWithEndQuoteAndSemicolon() {
-        myFixture.configureByFile("interfacesLocal/IERC20Local.sol")
-        myFixture.configureByFile("useCases/CompleteFolderWithEndQuoteAndSemicolon.sol")
-        val after = myFixture.configureByFile("useCases/CompleteFolderWithEndQuoteAndSemicolon_after.sol")
+    fun testImportCompletionPreviousFolderWithEndQuoteAndSemicolon() = checkCompletionParentDir(
+        "useCases/CompleteFolderWithEndQuoteAndSemicolon.sol",
+        "useCases/CompleteFolderWithEndQuoteAndSemicolon_after.sol"
+    )
 
-        checkResultAfterCompletion(after)
-    }
 
-    fun testImportCompletionPreviousFileWithEndQuoteAndSemicolon() {
-        myFixture.configureByFile("interfacesLocal/IERC20Local.sol")
-        myFixture.configureByFile("useCases/CompleteFileWithEndQuoteAndSemicolon.sol")
-        val after = myFixture.configureByFile("useCases/CompleteFileWithEndQuoteAndSemicolon_after.sol")
+    fun testImportCompletionPreviousFileWithEndQuoteAndSemicolon() = checkCompletionParentDir(
+        "useCases/CompleteFileWithEndQuoteAndSemicolon.sol", "useCases/CompleteFileWithEndQuoteAndSemicolon_after.sol"
+    )
 
-        checkResultAfterCompletion(after)
-    }
 
-    fun testImportCompletionPreviousFolderWithEndQuote() {
-        myFixture.configureByFile("interfacesLocal/IERC20Local.sol")
-        myFixture.configureByFile("useCases/CompleteFolderWithEndQuote.sol")
-        val after = myFixture.configureByFile("useCases/CompleteFolderWithEndQuote_after.sol")
+    fun testImportCompletionPreviousFolderWithEndQuote() = checkCompletionParentDir(
+        "useCases/CompleteFolderWithEndQuote.sol", "useCases/CompleteFolderWithEndQuote_after.sol"
+    )
 
-        checkResultAfterCompletion(after)
-    }
+    fun testImportCompletionPreviousFileWithEndQuote() = checkCompletionParentDir(
+        "useCases/CompleteFileWithEndQuote.sol", "useCases/CompleteFileWithEndQuote_after.sol"
+    )
 
-    fun testImportCompletionPreviousFileWithEndQuote() {
-        myFixture.configureByFile("interfacesLocal/IERC20Local.sol")
-        myFixture.configureByFile("useCases/CompleteFileWithEndQuote.sol")
-        val after = myFixture.configureByFile("useCases/CompleteFileWithEndQuote_after.sol")
+    fun testImportCompletionPreviousFolderOnNamedImportWithEndQuoteAndSemicolon() = checkCompletionParentDir(
+        "useCases/CompleteFolderOnNamedImportWithEndQuoteAndSemicolon.sol",
+        "useCases/CompleteFolderOnNamedImportWithEndQuoteAndSemicolon_after.sol"
+    )
 
-        checkResultAfterCompletion(after)
-    }
+    fun testImportCompletionPreviousFileOnNamedImportWithEndQuoteAndSemicolon() = checkCompletionParentDir(
+        "useCases/CompleteFileOnNamedImportWithEndQuoteAndSemicolon.sol",
+        "useCases/CompleteFileOnNamedImportWithEndQuoteAndSemicolon_after.sol"
+    )
 
-    fun testImportCompletionWithEndQuoteAndSemicolonAfterCompletionPreviousFile3() {
-        myFixture.configureByFile("../interfaces/IERC20.sol")
-        checkResultAfterCompletion(
-            """import {IERC20} from "../inter/*caret*/";""", """import {IERC20} from "../interfaces";"""
-        )
-    }
+    fun testImportCompletionPreviousFolderOnNamedImportWithEndQuote() = checkCompletionParentDir(
+        "useCases/CompleteFolderOnNamedImportWithEndQuote.sol",
+        "useCases/CompleteFolderOnNamedImportWithEndQuote_after.sol"
+    )
 
-    fun testImportCompletionWithEndQuoteAndSemicolonAfterCompletionPreviousFile4() {
-        myFixture.configureByFile("../interfaces/IERC20.sol")
-        checkResultAfterCompletion(
-            """import {IERC20} from "../interfaces/IE/*caret*/";""",
-            """import {IERC20} from "../interfaces/IERC20.sol";"""
-        )
-    }
-
-    fun testImportCompletionWithEndQuoteAfterCompletionPreviousFile3() {
-        myFixture.configureByFile("../interfaces/IERC20.sol")
-        checkResultAfterCompletion(
-            """import {IERC20} from "../inter/*caret*/"""", """import {IERC20} from "../interfaces""""
-        )
-    }
-
-    fun testImportCompletionWithEndQuoteAfterCompletionPreviousFile4() {
-        myFixture.configureByFile("../interfaces/IERC20.sol")
-        checkResultAfterCompletion(
-            """import {IERC20} from "../interfaces/IE/*caret*/"""",
-            """import {IERC20} from "../interfaces/IERC20.sol""""
-        )
-    }
+    fun testImportCompletionPreviousFileOnNamedImportWithEndQuote() = checkCompletionParentDir(
+        "useCases/CompleteFileOnNamedImportWithEndQuote.sol", "useCases/CompleteFileOnNamedImportWithEndQuote_after.sol"
+    )
 
     fun testImportCompletionPreservesMultipleParentSegments() {
         myFixture.configureByFile("interfacesLocal/IERC20Local.sol")
         myFixture.configureByFile("nested/feature/MultiParentImport.sol")
         myFixture.completeBasic()
-        val lookupStrings = myFixture.lookupElementStrings
-            ?: error("No lookup elements for import completion")
+        val lookupStrings = myFixture.lookupElementStrings ?: error("No lookup elements for import completion")
         assertTrue(
-            lookupStrings.any { it.contains("../../interfacesLocal/IERC20Local.sol") }
-        )
+            lookupStrings.any { it.contains("../../interfacesLocal/IERC20Local.sol") })
+    }
+
+    fun checkCompletionParentDir(beforePath: String, afterPath: String) {
+        myFixture.configureByFile("interfacesLocal/IERC20Local.sol")
+        myFixture.configureByFile(beforePath)
+        val after = myFixture.configureByFile(afterPath)
+
+        checkResultAfterCompletion(after)
     }
 
     override fun getTestDataPath() = "src/test/resources/fixtures/importCompletion/upDirImports/"
