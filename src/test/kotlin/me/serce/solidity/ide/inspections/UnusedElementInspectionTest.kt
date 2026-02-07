0c186a83a9ae6442e74012d5db54e5302e9f9858
Sergey Tselovalnikov <sergey@serce.me>
2026-02-07 14:29:08 +1100

    perf: optimize unused import usage detection

--- src/test/kotlin/me/serce/solidity/ide/inspections/UnusedElementInspectionTest.kt
+++ src/test/kotlin/me/serce/solidity/ide/inspections/UnusedElementInspectionTest.kt
@@ -23,6 +23,136 @@ class UnusedElementInspectionTest : SolInspectionsTestBase(UnusedElementInspecti
     """, checkWeakWarn = true)
   }
 
+  fun testUnusedImportAlias() {
+    InlineFile(
+      code = """
+        contract A {
+
+        }
+      """,
+      name = "A.sol"
+    )
+
+    checkByText(
+      """
+        /*@weak_warning descr="Unused import directive"@*/import {A as B} from "A.sol";/*@/weak_warning@*/
+        contract C {
+        }
+      """,
+      checkWeakWarn = true
+    )
+  }
+
+  fun testUsedImportAlias() {
+    InlineFile(
+      code = """
+        contract A {
+
+        }
+      """,
+      name = "A.sol"
+    )
+
+    checkByText(
+      """
+        import {A as B} from "A.sol";
+        contract C is B {
+        }
+      """,
+      checkWeakWarn = true
+    )
+  }
+
+  fun testUsedNamedImport() {
+    InlineFile(
+      code = """
+        contract A {
+
+        }
+      """,
+      name = "A.sol"
+    )
+
+    checkByText(
+      """
+        import {A} from "A.sol";
+        contract C is A {
+        }
+      """,
+      checkWeakWarn = true
+    )
+  }
+
+  fun testUsedNamedImportFreeFunction() {
+    InlineFile(
+      code = """
+        function foo() pure returns (uint) {
+          return 1;
+        }
+      """,
+      name = "A.sol"
+    )
+
+    checkByText(
+      """
+        import {foo} from "A.sol";
+        contract C {
+          function run() pure external returns (uint) {
+            return foo();
+          }
+        }
+      """,
+      checkWeakWarn = true
+    )
+  }
+
+  fun testUsedFileAliasImport() {
+    InlineFile(
+      code = """
+        contract A {
+
+        }
+      """,
+      name = "A.sol"
+    )
+
+    checkByText(
+      """
+        import * as X from "A.sol";
+        contract C is X.A {
+        }
+      """,
+      checkWeakWarn = true
+    )
+  }
+
+  fun testUsedTransitiveImport() {
+    InlineFile(
+      code = """
+        contract B {
+
+        }
+      """,
+      name = "B.sol"
+    )
+
+    InlineFile(
+      code = """
+        import "B.sol";
+      """,
+      name = "A.sol"
+    )
+
+    checkByText(
+      """
+        import "A.sol";
+        contract C is B {
+        }
+      """,
+      checkWeakWarn = true
+    )
+  }
+
   fun testUnusedElements() = checkByText("""
         int constant /*@weak_warning descr="Constant 'a' is never used"@*/a/*@/weak_warning@*/ = 1;
         int constant aa = 42;
