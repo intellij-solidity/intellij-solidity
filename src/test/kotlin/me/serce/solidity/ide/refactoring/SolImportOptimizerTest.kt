9cadd66cf3d4eceb1ec8e772016962ec04a85d2a
Sergey Tselovalnikov <sergey@serce.me>
2026-02-08 10:32:19 +1100

    import: fix no such element bug (#566)

--- src/test/kotlin/me/serce/solidity/ide/refactoring/SolImportOptimizerTest.kt
+++ src/test/kotlin/me/serce/solidity/ide/refactoring/SolImportOptimizerTest.kt
@@ -1,5 +1,7 @@
 package me.serce.solidity.ide.refactoring
 
+import com.intellij.openapi.command.WriteCommandAction
+import me.serce.solidity.ide.formatting.SolImportOptimizer
 import me.serce.solidity.utils.SolTestBase
 
 class SolImportOptimizerTest : SolTestBase() {
@@ -26,5 +28,84 @@ class SolImportOptimizerTest : SolTestBase() {
           }
       """, "OptimizeImports"
     )
+  }
+
+  fun testOptimiseImportsWithoutImports() {
+    checkEditorAction(
+      """
+          pragma solidity ^0.8.26;
+
+          contract C {
+            function a() external {}
+          }
+      """, //
+      """
+          pragma solidity ^0.8.26;
+
+          contract C {
+            function a() external {}
+          }
+      """, "OptimizeImports"
+    )
+  }
+
+  fun testLightOptimiseImportsSortsImports() {
+    InlineFile("contract A {}", "A.sol")
+    InlineFile("contract B {}", "B.sol")
+
+    checkByText(
+      """
+          pragma solidity ^0.8.26;
+
+          import {B} from "./B.sol";
+          import {A} from "./A.sol";
+
+          contract C is B {
+          }
+      """,
+      """
+          pragma solidity ^0.8.26;
+
+          import {A} from "./A.sol";
+          import {B} from "./B.sol";
+
+          contract C is B {
+          }
+      """
+    ) {
+      runOptimizer(fullOptimization = false)
+    }
+  }
+
+  fun testLightOptimiseImportsNoOpOnSingleImport() {
+    InlineFile("contract A {}", "A.sol")
+
+    checkByText(
+      """
+          pragma solidity ^0.8.26;
+
+          import {A} from "./A.sol";
+
+          contract C is A {
+          }
+      """,
+      """
+          pragma solidity ^0.8.26;
+
+          import {A} from "./A.sol";
+
+          contract C is A {
+          }
+      """
+    ) {
+      runOptimizer(fullOptimization = false)
+    }
+  }
+
+  private fun runOptimizer(fullOptimization: Boolean) {
+    val optimizer = SolImportOptimizer()
+    WriteCommandAction.runWriteCommandAction(project) {
+      optimizer.processFile(fixture.file, fullOptimization).run()
+    }
   }
 }
